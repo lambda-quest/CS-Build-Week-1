ddf708ef170e571afaa36b04c7c5a9e87d234526
Ronny Alvarado <rsalvarado777@gmail.com>
2020-01-22 14:24:18 -0600

    Work in progress - player movement

--- util/our_world.py
+++ util/our_world.py
@@ -4,18 +4,67 @@
 # PASTA
 
 
+class Player:
+    def __init__(self, world):
+        self.current_room = world[0][0]
+        self.world = world
+
+    def move_player(self, movement_key):
+        if movement_key == "n":
+            self.check_bounds(movement_key)
+            self.current_room = self.world[self.current_room.x][self.current_room.y + 1]
+        if movement_key == "e":
+            self.check_bounds(movement_key)
+            self.current_room = self.world[self.current_room.x +
+                                           1][self.current_room.y]
+        if movement_key == "s":
+            self.check_bounds(movement_key)
+            self.current_room = self.world[self.current_room.x][self.current_room.y - 1]
+        if movement_key == "w":
+            self.check_bounds(movement_key)
+            self.current_room = self.world[self.current_room.x -
+                                           1][self.current_room.y]
+
+    def check_bounds(self, movement_key):
+        if movement_key == "n":
+            if self.current_room.y < len(self.world)-1:
+                pass
+            else:
+                return "You cannot move up a level."
+        if movement_key == "e":
+            if self.current_room.x < len(self.world[0])-1:
+                pass
+            else:
+                return "You cannot move to the right."
+        if movement_key == "s":
+            if self.current_room.y > 0:
+                pass
+            else:
+                return "You cannot move down a level."
+        if movement_key == "w":
+            if self.current_room.x > 0:
+                pass
+            else:
+                return "You cannot move to the left."
+
+
 class Room:
     # def __init__(self, id, title, description, x, y):
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+    # def __init__(self, id, title, description):
+    def __init__(self, x, y):
+        # self.id = id
+        # self.title = title
+        # self.description = description
         # self.n_to = None
         # self.s_to = None
         # self.e_to = None
         # self.w_to = None
-        # self.x = x
-        # self.y = y
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        output = f'X: {self.x}, Y: {self.y}'
+        return output
 
 
 class World:
@@ -23,44 +72,25 @@ class World:
         self.x = x
         self.y = y
 
-    def generate_empty_world(self):
+    def generate_world(self):
         world = [None] * self.y
         for i in range(len(world)):
             world[i] = [None] * self.x
         counter = 0
         for row in world:
-            for i in range(len(row)):
-                row[i] = counter
+            for column in range(len(row)):
+                row[column] = Room(row, column)
                 counter += 1
         return world
 
 
 world_instance = World(10, 10)
-new_world = world_instance.generate_empty_world()
-print(new_world)
+new_world = world_instance.generate_world()
+ronny = Player(new_world)
 
-
-# def create_realRoom(num):
-#     room = Room(
-#         title=f'Room Num:{num}', description="Generic", id=num
-#     )
-#     return room
-
-# def populate_world():
-#     created_world = createWorld(10,10)
-#     counter = 0
-
-#     for row in created_world:
-#         for column in row:
-#             print(f'PRE: {column}')
-#             # column = create_realRoom(counter)
-#             column = 'TACO JOHNSON'
-#             print(f'POST: {column}')
-#             # print(column.id)
-
-#             counter += 1
-#     return created_world
-
-
-# FINAL_output = populate_world()
-# print(FINAL_output)
+while(True):
+    print("Enter a movement: ")
+    x = input()
+    ronny.move_player(x)
+    print(
+        f'You are currently at [{ronny.current_room.x, ronny.current_room.y}]')
