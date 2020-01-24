e502bd3260fc90b3e1fe9823154527611083423d
MrT3313 <turgeon.dev@gmail.com>
2020-01-23 20:38:02 -0600

    updated JSON data on api/adv/init

--- adventure/api.py
+++ adventure/api.py
@@ -60,7 +60,8 @@ def initialize(request):
 
     # players = room.playerNames(player_id)
     # return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
-    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'players':players}, safe=True)
+    return JsonResponse({'uuid': uuid, 'name':player.user.username, 'id':room.id}, safe=True)
+    
 
 # @csrf_exempt
 @api_view(["POST"])
